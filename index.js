eecb1ea4a7dd1e75c7bc748c5265f84b141c5409
+ <+>
2024-09-27 12:24:12 +0300

    First class functions lab

---
+++ index.js
@@ -0,0 +1,25 @@
+function receivesAFunction(classes) {
+    return classes()
+}
+console.log(receivesAFunction(classes = () => 'OOP'))
+
+ function returnsANamedFunction() {
+    function namedFunction(day){
+    if (day === 'Monday'){
+        return 'Go to class'
+    }
+    else if (day === 'Tuesday'){
+        return 'Go for labs'
+    }
+    else {
+        return 'Go to work'
+    }
+}
+return namedFunction
+}
+
+function returnsAnAnonymousFunction() {
+    return () => 'MAGIC!'
+
+}
+console.log(returnsAnAnonymousFunction())
